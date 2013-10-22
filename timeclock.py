060610c0ed269dc7e87b6243d741949cdbfffeb3
Devon Campbell <raddevon@gmail.com>
2013-10-22 13:40:51 -0400

    Adds punch model and route

--- timeclock.py
+++ timeclock.py
@@ -1,10 +1,45 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+form datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
+
+class Punch(db.model):
+    name = db.Column(db.String(150))
+    time = db.Column(db.DateTime())
+    status = db.Column(db.Enum('in', 'out'))
+
+    def __init__(self, name, time):
+        self.name = name
+        self.time = time
+
+        previous_punch = Punch.query.filter_by(
+            name=name).order_by(Punch.time.desc()).first()
+
+        if previous_punch.status == 'in':
+            self.status = 'out'
+        else:
+            self.status = 'in'
+
+    def __repr__(self):
+        return '<Punch {} at {}>'.format(self.name, self.time.strftime('%m-%d-%y %H:%M'))
+
+
+@app.route('/punch/<name>')
+def punch(name):
+    new_punch = Punch(name, datetime.now())
+    previous_punch = Punch.query.filter_by(
+        name=name).order_by(Punch.time.desc()).first()
+    time_between = new_punch - previous_punch
+
+    if time_between > 120:
+        db.session.add(new_punch)
+        db.session.commit()
+    else:
+        return 'You punched too fast. Please wait at least 2 minutes before punching again.', 403
 if __name__ == "__main__":
     app.run()
