302168b5bb2fa299960fa22850b3c340fa0dbcf7
Devon Campbell <raddevon@gmail.com>
2013-10-22 12:48:15 -0400

    Renames to a more descriptive name

--- time-clock.py
+++ time-clock.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import DATABASE_URI
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
 
 if __name__ == "__main__":
     app.run()
