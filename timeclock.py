327810ddac0ab50c0bcf220b7aee82e0faee4f51
Devon Campbell <raddevon@gmail.com>
2013-10-22 12:57:00 -0400

    Fixes environment var name

--- timeclock.py
+++ timeclock.py
@@ -3,7 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 if __name__ == "__main__":
     app.run()
