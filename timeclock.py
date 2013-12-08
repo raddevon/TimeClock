0a40bc1d6ef0abe19883deb0fa3cc7430a953e01
Devon Campbell <raddevon@gmail.com>
2013-12-07 21:30:03 -0500

    Imports momentJS wrapper for Jinja

--- timeclock.py
+++ timeclock.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import distinct
+from moment import momentjs
 import os
 from datetime import datetime, timedelta
 
